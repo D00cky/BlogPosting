0dfa8b6693122f3ff6f3438712ec428091bc6131
d00cky <83293500+D00cky@users.noreply.github.com>
2026-02-02 06:45:42 -0300

    Finished the service, fixed the returning null, need to retrieve the user_id from the users table automatically based on the nickname.

--- src/main/java/com/blogPosting/Api/repository/UsersRepository.java
+++ src/main/java/com/blogPosting/Api/repository/UsersRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
     List<Users> findByEmail(String email);
-    List<Users> findByNickname(String name);
+    Optional<Users> findByNickname(String name);
 }
--- src/main/java/com/blogPosting/Api/service/PostService.java
+++ src/main/java/com/blogPosting/Api/service/PostService.java
@@ -6,6 +6,7 @@ import com.blogPosting.Api.entity.Post;
 import com.blogPosting.Api.entity.Users;
 import com.blogPosting.Api.repository.PostRepository;
 
+import com.blogPosting.Api.repository.UsersRepository;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,19 +16,23 @@ public class PostService {
 
     private final PostMapper postMapper;
     private final PostRepository postRepository;
+    private final UsersRepository usersRepository;
 
-    public PostService(PostMapper postMapper, PostRepository postRepository) {
+    public PostService(PostMapper postMapper, PostRepository postRepository, UsersRepository usersRepository) {
         this.postMapper = postMapper;
         this.postRepository = postRepository;
+        this.usersRepository = usersRepository;
     }
 
     public Post createPost(@NotNull CreatePostsDTO postsDTO) {
         // check if the user exists in the Users table
-            if(postRepository.findUserById(postsDTO.user_id()).isEmpty()) {
+            if(usersRepository.findByNickname(postsDTO.author()) != null && usersRepository.findByNickname(postsDTO.author()).isEmpty()) {
+                throw new RuntimeException("Usuario nao encontrado");
+            } else {
                 Post CreatePost = postMapper.mapToPostCreation(postsDTO);
                 Post SavePost = postRepository.save(CreatePost);
                 ResponseEntity.ok("Post created");
+                return postMapper.mapToPostCreation(postsDTO);
             }
-        return postMapper.mapToPostCreation(postsDTO);
         }
     }
